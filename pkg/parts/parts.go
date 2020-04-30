4cf15a5db65a5c3430a8861da3262305c63be86f
Michael Kuehl <mickuehl@ratchet.cc>
2020-04-30 09:08:12 +0200

    merged files

--- pkg/parts/parts.go
+++ pkg/parts/parts.go
@@ -4,6 +4,27 @@ import (
 	log "github.com/majordomusio/log15"
 )
 
+type (
+	// ChannelFunc sets the pulse values of a channel
+	ChannelFunc func(int, int, int)
+
+	// ChannelCfg holds the configuration for controlling an actuator (e.g. servo or ESC)
+	ChannelCfg struct {
+		// channel number
+		N int
+		// min pulse length out of 4096
+		MinPulse int
+		// max pulse length out of 4096
+		MaxPulse int
+		// base puls to start from (min value)
+		BasePulse int
+		// zero pulse where the actor (e.g. servo) is in a neutral position
+		ZeroPulse int
+		// init pulse used to initialize/reset the channel. Set to -1 if not used
+		InitPulse int
+	}
+)
+
 var (
 	logger log.Logger
 )
