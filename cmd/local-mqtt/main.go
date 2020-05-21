cc973e2c9279752b92199d5836f125594de7d21f
Michael Kuehl <mickuehl@ratchet.cc>
2020-05-21 21:35:42 +0200

    dumping vehicle data to a csv file and some minor tweeks

--- cmd/local-mqtt/main.go
+++ cmd/local-mqtt/main.go
@@ -13,14 +13,17 @@ import (
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/majordomusio/commons/pkg/util"
 	log "github.com/majordomusio/log15"
 )
 
 var (
-	broker string
-	queue  string
-	cl     mqtt.Client
-	logger log.Logger
+	broker     string
+	queue      string
+	cl         mqtt.Client
+	logger     log.Logger
+	currentDir string
+	dumpFile   *os.File
 )
 
 func init() {
@@ -35,10 +38,54 @@ func shutdownHandler() {
 		os.Exit(1)
 	}
 	cl.Disconnect(250)
+
+	// close the dumpfile
+	if dumpFile != nil {
+		dumpFile.Sync()
+		dumpFile.Close()
+	}
 }
 
 func workerHandler() {
 	// FIXME do periodic work
+}
+
+//
+// Structure of the CSV file:
+//
+// TS, Batch, N, DeviceID, RecordingTS, TH, ST, HEAD
+//
+
+func dataFrameToCSVString(df *telemetry.DataFrame) string {
+	return fmt.Sprintf("%d,%d,%d,%s,%s,%s,%s,%s\n", df.TS, df.Batch, df.N, df.DeviceID, df.Data["recording_ts"], df.Data["th"], df.Data["st"], df.Data["head"])
+}
+
+func receiveDataFrame(client mqtt.Client, msg mqtt.Message) {
+	var df telemetry.DataFrame
+	err := json.Unmarshal(msg.Payload(), &df)
+	if err == nil {
+		if df.Type == telemetry.KV {
+			_, err := dumpFile.WriteString(dataFrameToCSVString(&df))
+			if err != nil {
+				logger.Error("Error dumping data to file", "err", err.Error())
+			}
+		} else {
+			if len(df.Blob) != 0 {
+				blob, err := base64.StdEncoding.DecodeString(df.Blob)
+				if err != nil {
+					logger.Error("Error unmarshalling a blob", "err", err.Error())
+				} else {
+					fn := fmt.Sprintf("%s/%d_%d.jpg", currentDir, df.Batch, df.N)
+					err := ioutil.WriteFile(fn, blob, 0644)
+					if err != nil {
+						logger.Error("Error dumping blob to file", "file", fn, "err", err.Error())
+					}
+				}
+			}
+		}
+	} else {
+		logger.Error("Error unmarshalling a dataframe", "err", err.Error())
+	}
 }
 
 func main() {
@@ -55,12 +102,27 @@ func main() {
 		os.Exit(1)
 	}()
 
-	// setup and configuration
+	// prepare the datadump location
+	currentDir = fmt.Sprintf("dump/%d", util.Timestamp())
+	err := os.MkdirAll(currentDir, 0755)
+	if err != nil {
+		logger.Error("Error creating the data directory", "dir", currentDir, "err", err.Error())
+		os.Exit(1)
+	}
+
+	// datadump file
+	dumpFile, err = os.Create(currentDir + "/data.csv")
+	if err != nil {
+		logger.Error("Error creating the data file", "file", currentDir+"/data.csv", "err", err.Error())
+		os.Exit(1)
+	}
+
+	// MQTT setup and configuration
 	opts := mqtt.NewClientOptions().AddBroker(broker).SetClientID("mqtt-local")
 	opts.SetKeepAlive(2 * time.Second)
 	opts.SetPingTimeout(1 * time.Second)
 
-	// create a client
+	// create a MQTT client and subscribe to the queue
 	cl = mqtt.NewClient(opts)
 	if token := cl.Connect(); token.Wait() && token.Error() != nil {
 		logger.Error("Error connecting to broker", "err", token.Error())
@@ -72,37 +134,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	logger.Info("Starting")
+	logger.Info("Ready to receive data", "queue", queue, "broker", broker)
 
 	// periodic background processes
 	backgroundChannel := time.NewTicker(time.Second * time.Duration(10)).C
 	for {
 		<-backgroundChannel
 		workerHandler()
-	}
-}
-
-func receiveDataFrame(client mqtt.Client, msg mqtt.Message) {
-	var df telemetry.DataFrame
-	err := json.Unmarshal(msg.Payload(), &df)
-	if err == nil {
-		if df.Type == telemetry.KV {
-			logger.Debug("dataframe", "data", df)
-		} else {
-			if len(df.Blob) != 0 {
-				blob, err := base64.StdEncoding.DecodeString(df.Blob)
-				if err != nil {
-					logger.Error("Error unmarshalling a blob", "err", err.Error())
-				} else {
-					fn := fmt.Sprintf("./data/%d_%d.jpg", df.Batch, df.N)
-					err := ioutil.WriteFile(fn, blob, 0644)
-					if err != nil {
-						logger.Error("Error dumping blob to file", "file", fn, "err", err.Error())
-					}
-				}
-			}
-		}
-	} else {
-		logger.Error("Error unmarshalling a dataframe", "err", err.Error())
 	}
 }
